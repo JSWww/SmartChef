e91803af50bf402d1ef8062ace56ae334f003c6b
JSWww <tjddnjs9497@naver.com>
2019-05-10 00:09:42 +0900

    commit

--- app/src/main/java/com/ssu/smartchef/LoginActivity.java
+++ app/src/main/java/com/ssu/smartchef/LoginActivity.java
@@ -1,39 +1,21 @@
-package com.ssu.smartchef.ui.login;
+package com.ssu.smartchef;
 
-import android.app.Activity;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -42,13 +24,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.ssu.smartchef.MainActivity;
-import com.ssu.smartchef.R;
-import com.ssu.smartchef.SignUpDialog;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
-
-    private LoginViewModel loginViewModel;
 
     /* add code */
     private Button googleLoginButton;
@@ -65,92 +42,10 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        });
 
         /* add code */
-        Button signUpButton = findViewById(R.id.signUp);
+//        Button signUpButton = findViewById(R.id.signUp);
 
         // [START config_signin]
         // Configure Google Sign In
@@ -166,16 +61,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
-    }
-
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
-
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
     /* add code */
@@ -301,24 +186,24 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 //    @Override
     public void onClick(View v) {
         int i = v.getId();
-        if (i == R.id.signUp) {
-            SignUpDialog signUpDialog = new SignUpDialog(LoginActivity.this);
-            signUpDialog.setContentView(R.layout.signup_dialog);
-
-            // 디바이스 가로 세로 길이 가져오기
-            DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
-            int width = dm.widthPixels;
-            int height = dm.heightPixels;
-
-            // 다이얼로그 크기 조절하기
-            WindowManager.LayoutParams layoutParams = signUpDialog.getWindow().getAttributes();
-            layoutParams.copyFrom(signUpDialog.getWindow().getAttributes());
-            layoutParams.width = width - width / 3;
-            layoutParams.height = height / 3;
-            signUpDialog.getWindow().setAttributes(layoutParams);
-
-            signUpDialog.show();
-        }
+//        if (i == R.id.signUp) {
+//            SignUpDialog signUpDialog = new SignUpDialog(LoginActivity.this);
+//            signUpDialog.setContentView(R.layout.signup_dialog);
+//
+//            // 디바이스 가로 세로 길이 가져오기
+//            DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+//            int width = dm.widthPixels;
+//            int height = dm.heightPixels;
+//
+//            // 다이얼로그 크기 조절하기
+//            WindowManager.LayoutParams layoutParams = signUpDialog.getWindow().getAttributes();
+//            layoutParams.copyFrom(signUpDialog.getWindow().getAttributes());
+//            layoutParams.width = width - width / 3;
+//            layoutParams.height = height / 3;
+//            signUpDialog.getWindow().setAttributes(layoutParams);
+//
+//            signUpDialog.show();
+//        }
 
 //        else if (i == R.id.signOutButton) {
 //            signOut();
