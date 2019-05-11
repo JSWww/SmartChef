ec6366b88fa5f61464755d9b714c378b78f8c6cf
JSWww <tjddnjs9497@naver.com>
2019-05-11 23:08:14 +0900

    activity order control

---
+++ app/src/main/java/com/ssu/smartchef/EmailLoginActivity.java
@@ -0,0 +1,24 @@
+package com.ssu.smartchef;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class EmailLoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+
+
+    }
+}
--- app/src/main/java/com/ssu/smartchef/googleSignUpActivity.java
+++ app/src/main/java/com/ssu/smartchef/googleSignUpActivity.java
@@ -1,7 +1,12 @@
 package com.ssu.smartchef;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class googleSignUpActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class googleSignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_up);
+
+        TextView emailText = findViewById(R.id.emailText);
+        final TextView nickNameText = findViewById(R.id.nickNameText);
+        Button okButton = findViewById(R.id.emailAuthButton);
+
+        Intent intent =  getIntent();
+        String email = intent.getStringExtra("email");
+        emailText.setText(email);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (nickNameText.getText().toString().length() < 3) {
+                    Toast.makeText(getApplicationContext(), "닉네임은 최소 4문자 이상이어야 합니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent mainActivityIntent = new Intent(googleSignUpActivity.this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                    LoginActivity loginActivity = (LoginActivity)LoginActivity.loginActivity;
+                    loginActivity.finish();
+                    finish();
+                }
+            }
+        });
     }
+
+
 }
