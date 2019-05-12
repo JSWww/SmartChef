2490904eb3bdb58ad6b1698e7d710051bb8cfee3
jissi <ji5ji@naver.com>
2019-05-12 21:54:53 +0900

    demo version

--- app/src/main/java/com/ssu/smartchef/MainActivity.java
+++ app/src/main/java/com/ssu/smartchef/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ssu.smartchef;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -136,22 +137,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Intent intent;
         int id = item.getItemId();
-/*
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.regist) {
+            intent = new Intent(this, RegistRecipe.class);
+            startActivity(intent);
+            //finish();
+        } else if (id == R.id.cookscale) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.cookhelp) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.setting) {
 
         }
-*/
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
