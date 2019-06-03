e9212e129edd8bf537896f3190d8f86a5b8e47a4
jswww <tjddnjs9497@naver.com>
2019-06-03 14:40:53 +0900

    regist menu hide when logout

--- app/src/main/java/com/ssu/smartchef/activities/StepExplainActivity.java
+++ app/src/main/java/com/ssu/smartchef/activities/StepExplainActivity.java
@@ -1,21 +1,18 @@
 package com.ssu.smartchef.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.ssu.smartchef.R;
 import com.ssu.smartchef.adapters.IngredientAdapter;
-import com.ssu.smartchef.adapters.mainAdapter;
-import com.ssu.smartchef.data.IngredientData;
 import com.ssu.smartchef.data.RecipeStepData;
 
 import java.util.ArrayList;
@@ -108,7 +105,7 @@ public class StepExplainActivity extends AppCompatActivity {
         stepTitle.setText(stepList.get(index).getStepTitle());
         stepExplain.setText(stepList.get(index).getStepExplain());
         adapter.listData = stepList.get(index).getIngredientArrayList();
-        if(index == 0 && ingredientIndex == 0 && isScale == true){
+        if(index == 0 && ingredientIndex == 0 && isScale == false){
             pre.setVisibility(View.INVISIBLE);
         }
         else{
